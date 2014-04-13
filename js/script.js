97a83fceeb9178993dead2ccb45cb36fe8a89291
puhrome <phirom.yim@fullsail.edu>
2014-04-13 15:49:59 -0400

    Strings and Booleans
    Understanding how to use Strings and Booleans in WebStorm. Using \ as
    escape functions and \n as new line.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Strings and Booleans Created by Phirom Yim on 4/13/14.
+ */
+var age = 31; //number variable
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "Phirom Yim";//String Variable in "  "
+//quotes separate literal string from variable of the same name
+
+var phrase = "I don't know"; //don't use single quote, it could confuse machine
+//but if you use singe quotations ' ' use \ i.e. don\t
+//new line character \n
+
